a7ddd3025f8e872ce500b6e5940e47f9e395118f
hugo-ravailhe <hugo.rh@free.fr>
2023-07-21 17:40:26 +0200

    Update MlbApiSpec.scala

--- src/test/scala/mlb/MlbApiSpec.scala
+++ src/test/scala/mlb/MlbApiSpec.scala
@@ -2,19 +2,21 @@ package mlb
 
 import munit._
 import zio.http._
+import zio.*
 
 class MlbApiSpec extends munit.ZSuite {
 
   val app: App[Any] = mlb.MlbApi.static
 
-  testZ("should be ok") {
-
-    val req = Request.get(URL(Root / "Hello MLB Fans!"))
-    assertZ(app.runZIO(req).isSuccess)
-  }
-
-  testZ("should be ko") {
+  testZ("GET / - 404") {
     val req = Request.get(URL(Root))
     assertZ(app.runZIO(req).isFailure)
   }
+
+  testZ("GET /text - Hello MLB Fans!") {
+    val req = Request.get(URL(Root / "text"))
+    assertEqualsZ(app.runZIO(req), Response.text("Hello MLB Fans!"))
+  }
+
+  
 }
